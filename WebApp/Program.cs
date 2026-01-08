968c07ab4f457cee5a490e5faa1cb03e19ae846b
SanketNyaynite <sanket.nyaynite@gmail.com>
2026-01-07 18:39:44 -0600

    Add HttpContext response object

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -7,6 +7,19 @@ var builder = WebApplication.CreateBuilder(args);       //sets up Kestrel server
  */
 var app = builder.Build();                              //generates the instance of web application.
 
-app.MapGet("/", () => "Hello World!");                  //corresponds to the middleware component and processes HTTP requests.Lamba function
+//Below is a Http request Run method that handles all HTTP requests.
+app.Run(async (HttpContext context) =>                 //corresponds to the middleware component and processes HTTP requests.Lamba function
+{
+    await context.Response.WriteAsync($"The method is: {context.Request.Method}\r\n");
+    await context.Response.WriteAsync($"The url is: {context.Request.Path}\r\n");
 
+    await context.Response.WriteAsync($"\r\nHeaders:\r\n");
+    foreach (var key in context.Request.Headers.Keys)
+    {
+       await context.Response.WriteAsync($"{key}: {context.Request.Headers[key]}\r\n");
+    }
+});
 app.Run();                                              //runs the web application as well as listens to kestrel server.
+
+
+    //WriteAsync is an asynchronous method that writes the specified string to the HTTP response body. So basically it is going to output data to response object and then when the reponse is sent to the browser, the browser is able to display that data.
